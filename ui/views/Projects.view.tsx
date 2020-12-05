eb15da4edb3d3137bf54aced33f45cf7203dd71c
c00di <joakim.liukkonen@gmail.com>
2020-12-05 21:22:58 +0200

    Add Projects page.

---
+++ ui/views/Projects.view.tsx
@@ -0,0 +1,17 @@
+const Projects = () => {
+    return (
+        <>
+            <div className='projects-container'>
+                <h1>My Projects</h1>
+                <p>Coming soon(ish)...</p>
+            </div>
+
+            {/* STYLES */}
+            <style jsx>{`
+
+            `}</style>
+        </>
+    )
+}
+
+export default Projects
