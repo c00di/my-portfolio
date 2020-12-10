9b0eb3ae145adc928a73f4744b2f461afc80033e
c00di <joakim.liukkonen@gmail.com>
2020-12-10 21:35:47 +0200

    Change introduction.

--- ui/components/Introduction.comp.tsx
+++ ui/components/Introduction.comp.tsx
@@ -1,43 +1,53 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 const Introduction = () => {
-    return (
-        <>
-            <div className="container introduction-container">
-                <h2>
-                    I am a web developer with emphasis on various JavaScript <Link href='/tech'><a className='link'>technologies</a></Link>. 
+  return (
+    <>
+      <div className='container introduction-container'>
+        <h2>
+          I'm a web developer, with emphasis on various JavaScript{' '}
+          <Link href='/tech'>
+            <a className='link'>technologies</a>
+          </Link>
+          . Please check my{' '}
+          <Link href='/projects'>
+            <a className='link'>projects</a>
+          </Link>
+          {', '}
+          to see the cool stuff I have published.
+          {/* <br /><br />In 2020, after five years of living in Bangkok 🇹🇭, I moved to the capital city of my home country, Helsinki 🇫🇮. */}
+          <br />
+          <br />
+          Feel free to{' '}
+          <Link href='/contact'>
+            <a className='link'>contact</a>
+          </Link>{' '}
+          me.
+        </h2>
+      </div>
 
-                    Please check my <Link href='/projects'><a className='link'>projects</a></Link> to see the cool stuff I have published. 
+      {/* STYLES */}
+      <style jsx>{`
+        .introduction-container {
+          text-align: left;
+          max-width: 1000px;
+        }
 
-                    {/* <br /><br />In 2020, after five years of living in Bangkok 🇹🇭, I moved to the capital city of my home country, Helsinki 🇫🇮. */}
+        h2 {
+          letter-spacing: 6px;
+          line-height: 4vw;
+          font-weight: 400;
+          font-size: 2.3vw;
+        }
 
-
-                    <br /><br />Feel free to <Link href='/contact'><a className='link'>contact</a></Link> me.
-                </h2>
-            </div>
-
-            {/* STYLES */}
-            <style jsx>{`
-                .introduction-container {
-                    text-align: left;
-                    max-width: 1000px
-                }
-
-                h2 {
-                    letter-spacing: 6px;
-                    line-height: 4vw;
-                    font-weight: 400;
-                    font-size: 2.3vw;
-                }
-
-                a.link {
-                    color: cadetblue;
-                    text-decoration: underline;
-                    font-weight: 400;
-                }
-            `}</style>
-        </>
-    );
+        a.link {
+          color: cadetblue;
+          text-decoration: underline;
+          font-weight: 400;
+        }
+      `}</style>
+    </>
+  )
 }
 
-export default Introduction;
+export default Introduction
